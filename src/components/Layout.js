8ed33f0bef081054303f14c8787e341641f92e8d
hastyskunk127 <114715377+hastyskunk127@users.noreply.github.com>
2022-12-08 17:52:48 -0500

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,12 +11,12 @@ function Layout() {
             <Nav>
                 <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/'><strong>HOME</strong></Link></Nav.Link>
                 <div className='project-links'>
-                <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/Chomp'><strong>CHOMP</strong></Link></Nav.Link>
-                <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/Responsive'><strong>RESPONSIVE</strong></Link></Nav.Link>
-                <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/Iterative'><strong>ITERATIVE</strong></Link></Nav.Link>
-                <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/Development'><strong>DEVELOPMENT</strong></Link></Nav.Link>
-                <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/'><strong>WOORDWORKS</strong></Link></Nav.Link>
-                <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/'><strong>WRITING</strong></Link></Nav.Link>
+                <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/Portfolio/Chomp'><strong>CHOMP</strong></Link></Nav.Link>
+                <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/Portfolio/Responsive'><strong>RESPONSIVE</strong></Link></Nav.Link>
+                <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/Portfolio//Iterative'><strong>ITERATIVE</strong></Link></Nav.Link>
+                <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/Portfolio/Development'><strong>DEVELOPMENT</strong></Link></Nav.Link>
+                <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/Portfolio/'><strong>WOORDWORKS</strong></Link></Nav.Link>
+                <Nav.Link className="NavText" as={'div'}><Link style={{color: 'white', textDecoration: 'none'}} to='/Portfolio/'><strong>WRITING</strong></Link></Nav.Link>
                 </div>
             </Nav>
         </Navbar>
